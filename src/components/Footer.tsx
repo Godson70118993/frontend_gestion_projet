e412f99812a4c3be4cbdf2c88fa6dc0d5bf307a3
Godson Patam <godsonpatam4@gmail.com>
2025-07-16 07:48:45 +0000

    premier commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,36 @@
+import { Container, Facebook, Twitter, Youtube } from "lucide-react";
+
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-10 ">
+      <aside>
+        <Container className="w-10 h-10" />
+
+        <p className="font-bold">
+          GODSON
+          <span className="text-accent">DEV</span>
+        </p>
+        <p>Copyright © 2025 - Tous droits réservés</p>
+      </aside>
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+           <a href="https://twitter.com" target="_blank"
+            rel="noopener noreferrer">
+              <Twitter className="w-6 h-6 text-current" />
+            </a>
+            <a href="https:/lyoutube.com" target="_blank"
+            rel="noopener noreferrer">
+              < Youtube className="w-6 h-6 text-current" />
+            </a>
+            <a href="https://facebook.com" target="_blank"
+            rel="noopener noreferrer">
+            <Facebook className="w-6 h-6 text-current" />
+            </a>
+        </div>
+      </nav>
+    </footer>
+  )
+}
+
+
+export default Footer
---
+++ src/components/Home.tsx
@@ -0,0 +1,43 @@
+import { Mail } from "lucide-react"
+import img from '../assets/img.jpg'
+const Home = () => {
+  return (
+    <div id="Home" className="flex flex-col-reverse md:flex-row justify-center 
+    items-center md:my-32 my-10">
+    
+       <div className="flex flex-col "> 
+            <h1 className="text-5xl md:text-6xl 
+            font-bold text-center md:text-left
+            mt-10 md:mt-0">
+               Bonjour 👋, <br /> je suis{""} 
+               <span 
+               className="text-accent"> GodsonDev</span> 
+            </h1>
+            <p className="my-4 text-md text-center
+            md:text-left">
+                Je suis un développeur full-stack <br /> 
+                avec 2 ans d'expérience, utilisant React <br /> et Node.js.
+                Contactez-moi si vous avez besoin de mes services.
+            </p>
+            <a href="" className="btn btn-accent md:w-fit">
+                <Mail className="w-5 h-5" />
+                contactez-moi 
+            </a>
+       </div>
+            
+       <div className="md:ml-60">
+                
+            <img src={img} alt="" className="w-96 h-96 object-cover
+            border-8 border-accent shadow-xl" 
+            style={{
+                borderRadius: "33% 70% 70% 30% / 67% 62% 38% 33%"
+            }}
+            />
+
+       </div>
+
+    </div>
+  )
+}
+
+export default Home
